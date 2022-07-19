2e814508c1e1463c88408e19cb7c9e7887a2298f
Ewerton Bezerra <ewerton_tavares@live.com>
2022-07-18 21:23:09 -0300

    modulo-1-post

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import './App.css';
+import './global.css';
+import styles from './App.module.css';
+import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <Post
-          author="Ewerton Tavares"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quo maxime doloremque voluptates deleniti repudiandae, quas delectus necessitatibus non eligendi deserunt nihil ut natus inventore id praesentium quos, quia enim."
-        />
-        <Post
-          author="Jeff Bezos"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quo maxime doloremque voluptates deleniti repudiandae, quas delectus necessitatibus non eligendi deserunt nihil ut natus inventore id praesentium quos, quia enim."
-        />
-      </header>
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Ewerton Tavares"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quo maxime doloremque voluptates deleniti repudiandae, quas delectus necessitatibus non eligendi deserunt nihil ut natus inventore id praesentium quos, quia enim."
+          />
+          <Post
+            author="Jeff Bezos"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quo maxime doloremque voluptates deleniti repudiandae, quas delectus necessitatibus non eligendi deserunt nihil ut natus inventore id praesentium quos, quia enim."
+          />
+        </main>
+      </div>
     </div>
   );
 }
